a291912e561cbd3d688f809c03700e00245a0eca
petrus nguyen thai hoc <hoc081098@gmail.com>
2022-08-01 23:20:00 +0700

    no_leading_underscores_for_local_identifiers

--- test/loader_bloc_test.dart
+++ test/loader_bloc_test.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, no_leading_underscores_for_local_identifiers
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_loader/src/loader_bloc.dart';
